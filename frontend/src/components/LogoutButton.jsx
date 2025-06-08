d5e3cf3ddb1849507e23b71f61475d83555cf902
Khan Shoaib <Khanshoaibishtiyak@gmail.com>
2025-06-08 23:26:40 +0530

    deploy

--- frontend/src/components/LogoutButton.jsx
+++ frontend/src/components/LogoutButton.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useAuthStore } from '../store/useAuthStore'
-
+import { useNavigate } from 'react-router-dom'
 
 const LogoutButton = ({children}) => {
+  const navigate = useNavigate();
     const {logout} = useAuthStore();
     const onLogout = async() => {
         await logout();
+      navigate('/');
     }
 
   return (
